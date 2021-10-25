5e3ee1b417ad76161bf99897ad3639343b3c6f6f
Opilane TTHK <opilane@tthk.ee>
2021-10-25 10:52:20 +0300

    SQL

---
+++ Marko/Models/SQLDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Marko.Models
+{
+    public class SQLDbContext : DbContext 
+    {
+
+        public SQLDbContext(DbContextOptions<SQLDbContext> options)
+            : base(options) { }
+
+        // näide, kuidas teha, kui lisate domaini alla ühe objekti
+        // migratsioonid peavad tulema siia libary-sse e TARge20.Data alla.
+        public DbSet<Employee> Employee { get; set; }
+    }
+}
